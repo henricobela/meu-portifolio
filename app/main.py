f654a86186a1bbc008934bdd0ba13f03d7b64a16
henricobela <henrico.developer@gmail.com>
2024-04-02 14:42:10 -0300

    requirements and menu

--- app/main.py
+++ app/main.py
@@ -4,11 +4,9 @@ import pandas as pd
 from streamlit_option_menu import option_menu
 
 
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
 
+selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
 
 st.header(selected)
 
