7127eef0481c34e3a8e15a0cf851b6f13033ce4f
henricobela <henrico.developer@gmail.com>
2024-04-02 14:39:08 -0300

    requirements and menu

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from streamlit_option_menu import option_menu
 
+
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
 
 
 st.header("Portfolio")
+
+
