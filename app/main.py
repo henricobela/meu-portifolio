b091dee973ff4ef7e6c3ec16bb9f1c0cb8662a19
henricobela <henrico.developer@gmail.com>
2024-04-03 11:38:41 -0300

    pages init

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from utils.config_page import default_set_page_config
-from streamlit_option_menu import option_menu
+from streamlit_navigation_bar import st_navbar
 from page_functions.home_page import home
 from page_functions.about_me_page import about_page
 from page_functions.model_breast_page import breast_page
@@ -13,10 +13,8 @@ from page_functions.settings_page import settings
 default_set_page_config()
 
 
-selected = option_menu("linkedin.com/in/henricobela", 
-                       ["Home", "About me", "Model 1", "Model 2", "Model 3", 'Settings', 'Contact', ], 
-        icons=['house', 'terminal', 'activity', 'eye', 'currency-dollar', 'gear', 'telephone-forward'], menu_icon="linkedin", default_index=1,
-        orientation="horizontal")
+selected = st_navbar(["Home", "About me", "Model 1", "Model 2", "Model 3", 'Settings', 'Contact', ], 
+               icons=['house', 'terminal', 'activity', 'eye', 'currency-dollar', 'gear', 'telephone-forward'])
 
 
 if selected == "Home":
