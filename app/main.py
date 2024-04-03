309af5b24924e15e17e36a6e58b5230281e8f755
henricobela <henrico.developer@gmail.com>
2024-04-03 10:24:10 -0300

    Options added

--- app/main.py
+++ app/main.py
@@ -9,14 +9,18 @@ from streamlit_option_menu import option_menu
 default_set_page_config()
 
 
-selected = option_menu("Main Menu", ["Home", "Model 1", "Model 2", "Model 3", 'Settings', ], 
-        icons=['house', 'activity', 'eye', 'currency-dollar','gear'], menu_icon="cast", default_index=1,
+selected = option_menu("Main Menu", ["Home", "About me", "Model 1", "Model 2", "Model 3", 'Settings', 'Contact', ], 
+        icons=['house', 'terminal', 'activity', 'eye', 'currency-dollar', 'gear', 'telephone-forward'], menu_icon="cast", default_index=1,
         orientation="horizontal")
 
 
 if selected == "Home":
         st.header(selected)
         st.subheader("Here we are Home")
+
+elif selected == "About me":
+        st.header(selected)
+        st.subheader("Here we are about me")
 
 elif selected == "Model 1":
         st.header(selected)
@@ -34,7 +38,11 @@ elif selected == "Settings":
         st.header(selected)
         st.subheader("Here we are Settings")
 
-#todo: listagem das opções do menu
+elif selected == "Contact":
+        st.header(selected)
+        st.subheader("Here we are Contact")
+
+
 
 
 
