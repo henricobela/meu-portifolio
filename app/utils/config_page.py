c8abe9716e01b5dfdfd471720d634756aed1da6a
henricobela <henrico.developer@gmail.com>
2024-04-03 12:48:53 -0300

    nav bar logo input

--- app/utils/config_page.py
+++ app/utils/config_page.py
@@ -3,5 +3,5 @@ import streamlit as st
 def default_set_page_config():
     st.set_page_config(
         initial_sidebar_state = "collapsed",
-        page_icon = "utils/imgs/logotipo.png",
+        page_icon = "utils/imgs/logotipo_sem_escrito.svg",
         page_title = "Portfolio", layout = "wide")
