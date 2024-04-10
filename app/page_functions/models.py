9e748e53e86256f3ac9e565ef1b4b2a22003a40e
henricobela <henrico.developer@gmail.com>
2024-04-10 11:31:15 -0300

    Models Page todo

--- app/page_functions/models.py
+++ app/page_functions/models.py
@@ -17,4 +17,8 @@ def models_page():
     with tab_rentals:
         st.text("Rentals")        
 
+    #todo: decide which is better, tabs or select box, then, do the page models, then, do the models
+    #todo: do the page models
+    #todo: do the models
+
 
