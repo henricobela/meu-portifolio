a1cdd270565fcaf3bce37b73ad95f5e89d79a4e7
henricobela <henrico.developer@gmail.com>
2024-04-12 17:33:24 -0300

    model done, and model page breast init

--- app/page_functions/model_breast_page.py
+++ app/page_functions/model_breast_page.py
@@ -14,11 +14,18 @@ def breast_page():
         for line in f:
             feature_name = line.strip()
             feature_names.append(feature_name)
+    # for f_name in feature_names:
+    #     value = st.slider(f"Please select a value for {f_name}: ", min_value = df[f_name].min(), max_value = df[f_name].max())
+    #     data.append(value)
 
 
-    for f_name in feature_names:
-        value = st.slider(f"Please select a value for {f_name}: ", min_value = df[f_name].min(), max_value = df[f_name].max())
-        data.append(value)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    # Iterar sobre as colunas do DataFrame e adicionar controles deslizantes a cada coluna
+    for i, (nome_coluna, serie) in enumerate(df.iteritems()):
+        with locals()[f'col{(i % 5) + 1}']:
+            st.slider(nome_coluna, min_value=serie.min(), max_value=serie.max(), value=(serie.min(), serie.max()))
 
     # data = (11.76,21.6,74.72,427.9,0.08637,0.04966,0.01657,0.01115,0.1495,0.05888,0.4062,1.21,2.635,28.47,0.005857,0.009758,0.01168,0.007445,0.02406,0.001769,12.98,25.72,82.98,516.5,0.1085,0.08615,0.05523,0.03715,0.2433,0.06563)
     model = Model("app/models/breast/svc_model.pkl")
