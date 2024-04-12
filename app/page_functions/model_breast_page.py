45753067eb12c3d3127e04ccb61e7a7d8fec2a6c
henricobela <henrico.developer@gmail.com>
2024-04-12 17:38:12 -0300

    model done, and model page breast init

--- app/page_functions/model_breast_page.py
+++ app/page_functions/model_breast_page.py
@@ -20,12 +20,16 @@ def breast_page():
 
 
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+    num_cols = 6
+    num_features = len(feature_names)
+    num_rows = -(-num_features // num_cols)  # Calcula o número de linhas necessárias
 
-    # Iterar sobre as colunas do DataFrame e adicionar controles deslizantes a cada coluna
-    for i, (nome_coluna, serie) in enumerate(df.items()):
-        with locals()[f'col{(i % 5) + 1}']:
-            st.slider(nome_coluna, min_value=serie.min(), max_value=serie.max(), value=(serie.min(), serie.max()))
+    for row in range(num_rows):
+        cols = st.columns(num_cols)  # Cria uma linha com várias colunas
+        for col_index, f_index in enumerate(range(row * num_cols, min((row + 1) * num_cols, num_features))):
+            f_name = feature_names[f_index]
+            value = cols[col_index].slider(f"Please select a value for {f_name}: ", min_value=df[f_name].min(), max_value=df[f_name].max())
+            data.append(value)
 
     # data = (11.76,21.6,74.72,427.9,0.08637,0.04966,0.01657,0.01115,0.1495,0.05888,0.4062,1.21,2.635,28.47,0.005857,0.009758,0.01168,0.007445,0.02406,0.001769,12.98,25.72,82.98,516.5,0.1085,0.08615,0.05523,0.03715,0.2433,0.06563)
     model = Model("app/models/breast/svc_model.pkl")
