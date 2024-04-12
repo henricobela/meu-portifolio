606fee3e4a9612a1212a1908aa165c947e7118cb
henricobela <henrico.developer@gmail.com>
2024-04-12 17:17:50 -0300

    model done, and model page breast init

--- app/page_functions/model_breast_page.py
+++ app/page_functions/model_breast_page.py
@@ -1,8 +1,26 @@
 import streamlit as st
+import pandas as pd
 from models.breast.breast_model import *
 
+
 def breast_page():
-    data = (11.76,21.6,74.72,427.9,0.08637,0.04966,0.01657,0.01115,0.1495,0.05888,0.4062,1.21,2.635,28.47,0.005857,0.009758,0.01168,0.007445,0.02406,0.001769,12.98,25.72,82.98,516.5,0.1085,0.08615,0.05523,0.03715,0.2433,0.06563)
+
+    feature_names_path = "feature_names.txt"
+    feature_names = []
+    data = ()
+    df = pd.read_csv("values_min_max.csv")
+    with open(feature_names_path, 'r') as f:
+        for line in f:
+            feature_name = line.strip()
+            feature_names.append(feature_name)
+
+
+    for f_name in feature_names:
+        value = st.slider(f"Please select a value for {f_name}: ", min_value = df[f_name].min(), max_value = df[f_name].max(),
+                          value = (df[f_name].min(), df[f_name].max()))
+        data.append(value)
+
+    # data = (11.76,21.6,74.72,427.9,0.08637,0.04966,0.01657,0.01115,0.1495,0.05888,0.4062,1.21,2.635,28.47,0.005857,0.009758,0.01168,0.007445,0.02406,0.001769,12.98,25.72,82.98,516.5,0.1085,0.08615,0.05523,0.03715,0.2433,0.06563)
     model = Model("app/models/breast/svc_model.pkl")
     y_pred = model.predict(data)
     st.header(y_pred)
--- todo.py
+++ todo.py
@@ -1,2 +1,1 @@
-#continue breast model development
 #do the breast page with input data to select by users
